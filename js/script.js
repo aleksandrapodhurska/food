95e43758f73722e90a24037f9dd0fb175a56240c
Aleks <chesneishaya93@gmail.com>
2021-03-13 18:37:41 +0200

    added slider

--- js/script.js
+++ js/script.js
@@ -339,6 +339,55 @@ window.addEventListener('DOMContentLoaded', () => {
                 
             } 
 
+            // Slider
+
+            const slides = document.querySelectorAll('.offer__slide'),
+                  prev = document.querySelector('.offer__slider-prev'),
+                  next = document.querySelector('.offer__slider-next'),
+                  current = document.querySelector('#current'),
+                  total = document.querySelector('#total');
+
+            let slideIndex = 1;
+
+            if (slides.length < 10) {
+                total.textContent = `0${slides.length}`;
+            } else {
+                total.textContent = `${slides.length}`;
+            }
+
+            showSlides(slideIndex);
+
+            function showSlides(n) {
+                if (n > slides.length) {
+                    slideIndex = 1;
+                }
+
+                if (n < 1) {
+                    slideIndex = slides.length;
+                }
+
+                slides.forEach(item => item.classList.add('hide'));
+                slides[slideIndex - 1].classList.remove('hide');
+
+                if (slides.length < 10) {
+                    current.textContent = `0${slideIndex}`;
+                } else {
+                    current.textContent = `${slideIndex}`;
+                }
+            }
+
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+
+            prev.addEventListener('click', () => {
+                plusSlides(-1);
+            });
+            
+            next.addEventListener('click', () => {
+                plusSlides(1);
+            });
+
 
 
  });
